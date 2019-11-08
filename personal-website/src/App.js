771b783e2f7532b8ab3fa8c8f4c36963b4af6bb7
Forest Elliott <forest@ip-83-246.wireless.oberlin.edu>
2019-11-07 20:59:04 -0500

    Started react app

---
+++ personal-website/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (<h1 id="title">Forest Elliott</h1>);
+  }
+}
+export default App;
